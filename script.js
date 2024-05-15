8e10436c36faf734ba57babf43914a9aaab44a2e
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-15 14:36:42 +0700

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+//ex1.1
+let waitingList = ['Sofia','David','Juan'];
+console.log(waitingList);
+//ex1.2
+waitingList.push('Sara','Augustin');
+waitingList.shift();
+console.log(waitingList);
+//ex1.3
+waitingList.splice(1, 0, "Renata");
+waitingList.push("Elena");
+console.log(waitingList);
